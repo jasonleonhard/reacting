3766d4521738729517ae69f7b3d89bee8164ea81
un5t0ppab13 <devbrights@gmail.com>
2015-10-04 18:32:47 -0700

    render 3 component props times

--- app/assets/javascripts/components/componentname.js.jsx
+++ app/assets/javascripts/components/componentname.js.jsx
@@ -9,18 +9,10 @@ var ComponentName = React.createClass({
             <li>Here is another property: {this.props.bla}</li>
             <li>"undefined" index.html.erb properies will simply not render like this: {this.props.notReal}</li>
           </ul>
-
-        <h1>You are truly {this.props.name}!</h1>
-          <ul>
-            <li>Here is another property: {this.props.bla}</li>
-            <li>"undefined" index.html.erb properies will simply not render like this: {this.props.notReal}</li>
-          </ul>
      </div>
- 		// <h1>Loreum {this.state.rows}!</h1>;
-		// <h1>Loreum {this.state.row}!</h1>;
 	 );
   }
 });
 
-// how to view this 
-// <%= react_component('HelloMessage', { name: 'Bob' }) %>
+// how to view this:
+  // < % = react_component('ComponentName', { name: 'un5t0ppab13', bla: 'bla bla bla' }) % >
