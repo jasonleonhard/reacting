10b9d45f2c6748817468318e91f8cf6ac5926d63
un5t0ppab13 <devbrights@gmail.com>
2016-07-18 15:58:25 -0700

    improve example component, use className not class, fix bootstrap require

--- app/assets/javascripts/components/componentname.js.jsx
+++ app/assets/javascripts/components/componentname.js.jsx
@@ -3,13 +3,24 @@ var ComponentName = React.createClass({
   render: function () {
     // properties
     return (
-      <div> 
-        <h1>You are truly {this.props.name}!</h1>
-          <ul>
-            <li>Here is another property: {this.props.bla}</li>
-            <li>"undefined" index.html.erb properies will simply not render like this: {this.props.notReal}</li>
-          </ul>
-     </div>
+      <div className="container dingo"> 
+
+        <div class="row">
+          <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+              <div class="caption">
+                <h1>You are truly {this.props.name}!</h1>
+              </div>
+
+              <ul>
+                <li>Here is another property: {this.props.bla}</li>
+                <li>"undefined" index.html.erb properies will simply not render like this: {this.props.notReal}</li>
+              </ul>
+
+            </div>
+          </div>
+        </div>
+      </div>
 	 );
   }
 });
